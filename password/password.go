5af3926c8fa260a9cb7ba29717b14d6f5f4890ff
Marius Kießling <vcs@mariuskiessling.de>
2019-06-19 22:08:31 +0200

    Move password generating logic out of store
    The logic handling generating new passwords is now located in its own go
    source file inside the password package.

--- password/password.go
+++ password/password.go
@@ -1,1 +1,93 @@
 package password
+
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
+)
+
+// A Generator is used to store all options needed to generate a password.
+type Generator struct {
+	Length            int
+	SpecialCharacters int
+	Numbers           int
+}
+
+var (
+	mapping = []byte{
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+		'A',
+		'B',
+		'C',
+		'D',
+		'E',
+		'F',
+		'G',
+		'H',
+		'I',
+		'J',
+		'K',
+		'L',
+		'M',
+		'N',
+		'O',
+		'P',
+		'Q',
+		'R',
+		'S',
+		'T',
+		'U',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z',
+	}
+)
+
+// Generate generates a password for the given Generator options and *n* alternative passwords.
+func (gen *Generator) Generate(numAlternatives int) (password string, alternatives []string) {
+	// We will not introduce a recursive function to generate a random string of
+	// the needed length due to the 'newly' introduced string builder. This
+	// dramatically reduces the number of memory operations needed.
+
+	pw := make([]byte, gen.Length)
+
+	// Step 1: Populate the password array only with random letters (both upper- and lower case)
+	for i := 0; i < gen.Length; i++ {
+		bigR, err := rand.Int(rand.Reader, big.NewInt(52))
+		r := bigR.Int64()
+		if err != nil {
+			log.Fatal("Something went completely wrong while generating random numbers...")
+		}
+
+		pw[i] = mapping[r]
+	}
+
+	return string(pw), nil
+}
--- password/store.go
+++ password/store.go
@@ -1,81 +1,14 @@
 package password
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"log"
-	"math/big"
 )
 
 // Store stores all generated passwords in an encrypted format.
 type Store struct {
 	passwords map[string][]string
 }
-
-// A Generator is used to store all options needed to generate a password.
-type Generator struct {
-	Length            int
-	SpecialCharacters int
-	Numbers           int
-}
-
-var (
-	mapping = []byte{
-		'a',
-		'b',
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'q',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'w',
-		'x',
-		'y',
-		'z',
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'H',
-		'I',
-		'J',
-		'K',
-		'L',
-		'M',
-		'N',
-		'O',
-		'P',
-		'Q',
-		'R',
-		'S',
-		'T',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Y',
-		'Z',
-	}
-)
 
 // Init initialises a new password store.
 func Init() (store *Store) {
@@ -99,26 +32,4 @@ func (store *Store) Remove(publicKey string, password string) {
 func (store *Store) Print() {
 	dump, _ := json.MarshalIndent(store.passwords, "", "  ")
 	fmt.Print(string(dump))
-}
-
-// Generate generates a password for the given Generator options and *n* alternative passwords.
-func (gen *Generator) Generate(numAlternatives int) (password string, alternatives []string) {
-	// We will not introduce a recursive function to generate a random string of
-	// the needed length due to the 'newly' introduced string builder. This
-	// dramatically reduces the number of memory operations needed.
-
-	pw := make([]byte, gen.Length)
-
-	// Step 1: Populate the password array only with random letters (both upper- and lower case)
-	for i := 0; i < gen.Length; i++ {
-		bigR, err := rand.Int(rand.Reader, big.NewInt(52))
-		r := bigR.Int64()
-		if err != nil {
-			log.Fatal("Something went completely wrong while generating random numbers...")
-		}
-
-		pw[i] = mapping[r]
-	}
-
-	return string(pw), nil
 }
